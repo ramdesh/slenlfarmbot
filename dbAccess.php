7242d243792ae161e47e3168663ae45600b76558
ramdesh <rasade88@gmail.com>
2015-08-09 17:11:25 +0530

    Moved bot to root

--- dbAccess.php
+++ dbAccess.php
@@ -6,12 +6,17 @@
  * 
  * @desc Class to handle database connections
  */
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
 class dbAccess {
 	protected static $dParams = array(
-						'host' 		=> 'localhost',
-						'user' 		=> 'rdeshapr_ramindu',
-						'password' 	=> '181mihiri',
-						'database'	=> 'rdeshapr_slenlfarmbot'
+						'host' 		=> DB_HOST,
+						'user' 		=> DB_USER,
+						'password' 	=> DB_PASS,
+						'database'	=> DB_NAME
 					);
 
 	
