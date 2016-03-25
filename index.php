1ea465eaeefb7c9ca217a6918c4cfbd12fda441f
CMNisal <cmnisal@gmail.com>
2016-03-25 21:21:52 +0530

    UsernameChange CMNisal -> Nisal

--- index.php
+++ index.php
@@ -95,7 +95,7 @@ function easter_eggs($farmer_name) {
         $reply .= urlencode('The Painkiller Army knows no boundaries. They know no fear. 
 ');
     }
-    if ($farmer_name == '@CMNisal') {
+    if ($farmer_name == '@Nisal') {
         $reply .= urlencode('Nisal will be coming for farming provided he doesn\'t get in to some freak accident on the way. 
 ');
     }
