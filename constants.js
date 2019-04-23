f001da06f046b03b985ffb0a3de031e147be5cf6
Ramindu Deshapriya <rasade88@gmail.com>
2019-04-23 17:02:14 -0400

    Added constants and mongo details

---
+++ constants.js
@@ -0,0 +1,8 @@
+const Constants = {
+  MONGO_URL: 'mongodb://slenl_admin:PGPUJ2TTAhgRwN9@ds145926.mlab.com:45926/slenlfarmbot',
+  DB_NAME: 'slenlfarmbot',
+  BOT_ACCESS_TOKEN: '112493740:AAEd4LNXzzkyrqhRwqJYHVnTcIypGjF8GU0',
+  MAINTENANCE_MSG: 'SL ENL Farm bot is down for maintenance as we move to better tech. Please contact RamdeshLota for more information and perceived completion dates.'
+
+};
+export default Constants;
--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-let https = require('https');
+import axios from 'axios';
+import mongodb from 'mongodb';
 
-const ACCESS_TOKEN = '112493740:AAHHFH1HHM95-HgSbVoibojcA6L3AsbhfaI';
-const MAINTENANCE_MSG = 'SL ENL Farm bot is down for maintenance as we move to better tech. Please contact @RamdeshLota for more information and perceived completion dates.';
+import Constants from './constants';
+
+const MongoClient = mongodb.MongoClient;
 
 exports.handler = async (req) => {
   function buildResponse(chatId, text) {
-    return 'https://api.telegram.org/bot'+ ACCESS_TOKEN + '/sendMessage?chat_id='
+    return 'https://api.telegram.org/bot'+ Constants.BOT_ACCESS_TOKEN + '/sendMessage?chat_id='
       + chatId + '&text=' + text;
   }
-  function sendHttp(url) {
-    https.get(url, resp => {
-      resp.on('end', () => {
-        console.log("request sent to tg");
-      });
-    }).on('error', err => {
-      console.error(err);
-    });
+  async function sendHttp(url) {
+    let response = await axios.get(url);
+    return {
+      "statusCode" : 200,
+      "body" : JSON.stringify({message: "This is the SL ENL Farm Bot"}),
+      "isBase64Encoded": false
+    };
   }
-  let chatId = req.message.chat.id;
-  sendHttp(encodeURI(buildResponse(chatId, MAINTENANCE_MSG)));
-  const response = {
-    statusCode: 200,
-    body: 'This is the Lambda version of the SL ENL Farm Bot',
+  let chatBody = JSON.parse(req.body);
+  if(chatBody.message.text.startsWith('/')) {
+    let chatId = chatBody.message.chat.id;
+    return sendHttp(encodeURI(buildResponse(chatId, Constants.MAINTENANCE_MSG)));
+  }
+  return {
+    "statusCode" : 200,
+    "body" : JSON.stringify({message: "This is the SL ENL Farm Bot"}),
+    "isBase64Encoded": false
   };
 
-  return response;
 };
